39b8378b36c1720105115d65ab78d0b8bc0c4a3e
mithil-iad <147080749+mithil-iad@users.noreply.github.com>
2026-02-25 18:44:05 +0000

    Add routing and component imports to App.js for navigation

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,6 +1,14 @@
 
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Activities from './components/Activities';
+import Leaderboard from './components/Leaderboard';
+import Teams from './components/Teams';
+import Users from './components/Users';
+import Workouts from './components/Workouts';
 import logo from './octofitapp-small.png';
+import './App.css';
 
 
 function App() {
